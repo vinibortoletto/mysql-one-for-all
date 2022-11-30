77bf9e04a633ba49d3a7ee2841f9ab69c3aebdc6
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-11-30 19:11:12 -0300

    refactor: requirement 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 SELECT 
-	COUNT(DISTINCT song.name) AS cancoes,
-    COUNT(DISTINCT artist.name) AS artistas,
-    COUNT(DISTINCT album.name) as albuns
-FROM  SpotifyClone.song AS song
-INNER JOIN SpotifyClone.artist AS artist
-ON song.artist_id = artist.id
-INNER JOIN SpotifyClone.album AS album
-ON song.album_id = album.id;
+	  COUNT(DISTINCT s.name) AS cancoes,
+    COUNT(DISTINCT a.name) AS artistas,
+    COUNT(DISTINCT al.name) as albuns
+FROM SpotifyClone.song AS s
+INNER JOIN SpotifyClone.artist AS a
+ON s.artist_id = a.id
+INNER JOIN SpotifyClone.album AS al
+ON s.album_id = al.id;
